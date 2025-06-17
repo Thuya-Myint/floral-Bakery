0afbe4549119e2050bc712ae218227f944ab547e
Thuya-Myint <165346326+Thuya-Myint@users.noreply.github.com>
2025-06-17 15:45:17 +0630

    fixed

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,46 +1,48 @@
 import React from 'react';
 import image1 from './assets/images/cake/macron/cakeLandingPage.jpg'
+// import image2 from './assets/images/flower/flower5.png'
 import image2 from './assets/images/flower/flower5.png'
 const Header = () => {
   return (
     <div>
-    <div className='w-screen h-screen flex relative'>
-      <div className='w-full h-full  bgImage1'>
-        
-      </div>
+      <div className='w-screen h-screen flex relative'>
+        <div className='w-full h-full  bgImage1'>
+
+        </div>
 
 
 
-      {/* <div className='w-1/2 h-full bgImage2'>
+        {/* <div className='w-1/2 h-full bgImage2'>
 
       </div> */}
 
         <div className='w-full h-full  absolute flex flex-col justify-center  items-center  text-white font-kugile'>
 
-        {/* <div className='w-full'>
+          {/* <div className='w-full'>
           <div className='w-[180px] h-[200px] bg-white flex justify-center ml-24 mt-20 -rotate-[20deg] shadow-xl shadow-black/40'>
           <img src={image1} alt="" className='w-[90%] h-[80%] mt-[5%]'/>
         </div>
         </div> */}
-        <div className='flex flex-col items-center bg-neutral-400/40 p-4 rounded-lg bg-Blur text-[2.5rem]'>
-          Shoon Shoon
-        <span className='px-4 font-shalyne'>
-          &
-        </span>
-        Amyint
+          <div className='flex flex-col items-center bg-neutral-400/40 p-4 rounded-lg bg-Blur text-[2.5rem]'>
+            Shoon Shoon
+            <span className='px-4 font-shalyne'>
+              &
+            </span>
+            Amyint
+          </div>
+        </div>
+
+      </div>
+      <div className='w-screen h-screen'>
+        <div className='w-[200px] h-[10%] relative imageCon'>
+          <img src={image2} alt="" className='' />
+          <div className='absolute top-0 textCon'>flower</div>
+        </div>
+        <div className='w-full h-15 bg-amber-100 text-center text-2xl pt-2'>
+          konnichiwa
         </div>
       </div>
-   
     </div>
-    <div className='w-screen h-screen bg-amber-200'>
-      <div>
-        <image src={image2} alt="" className='w-[100px] h-[100px]'/>
-      </div>
-       <div className='w-full h-15 bg-amber-100 text-center text-2xl pt-2'>
-        konnichiwa
-      </div>
-    </div>
-       </div>
   );
 }
 
